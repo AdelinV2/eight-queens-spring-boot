71823d3736d3b81dcde513679efaeb25a2375e86
Adelin <adelin10emil@gmail.com>
2025-01-08 11:00:37 +0200

    Initial commit

---
+++ eight-queens/src/main/java/com/eightqueens/eight_queens/EightQueensApplication.java
@@ -0,0 +1,13 @@
+package com.eightqueens.eight_queens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EightQueensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EightQueensApplication.class, args);
+	}
+
+}
---
+++ eight-queens/src/test/java/com/eightqueens/eight_queens/EightQueensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eightqueens.eight_queens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EightQueensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
