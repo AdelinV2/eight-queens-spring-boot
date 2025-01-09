1497f1e99c099b3c19756f782525e59eb642c464
Adelin <adelin10emil@gmail.com>
2025-01-09 11:32:37 +0200

    Created the model for solutions

---
+++ eight-queens/src/main/java/com/eightqueens/eight_queens/model/Solution.java
@@ -0,0 +1,14 @@
+package com.eightqueens.eight_queens.model;
+
+public class Solution {
+
+    private int[][] board;
+
+    public Solution(int[][] board) {
+        this.board = board;
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+}
